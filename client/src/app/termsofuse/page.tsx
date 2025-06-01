d82148bc59398e120f88d8d0d9b90301c8c789c3
duyethoang <hoangduyet300605@gmail.com>
2025-06-01 10:35:40 +0700

    chinhsachvadieukhoan,movies
    giao diện cơ bản, tạo folder

---
+++ client/src/app/termsofuse/page.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+const privacyPolicy = () => {
+  return (
+    <div className="pl-4">
+      <h1 className="flex-center text-white z-11 text-3xl font-bold mt-8 mb-4">ĐIỀU KHOẢN SỬ DỤNG</h1>
+      <p>Chào mừng và cảm ơn Quý Khách Hàng đã đến với website: ticknow.vn <br />
+      Website này thuộc quyền sở hữu và được quản lý bởi nhóm TickNow – sinh viên năm cuối Trường Cao đẳng FPT Polytechnic Hồ Chí Minh, địa chỉ tại Công viên Phần mềm Quang Trung, Quận 12, TP. Hồ Chí Minh. <br />
+      Website được sử dụng cho các hoạt động đặt vé xem phim và cung cấp thông tin liên quan đến hệ thống rạp chiếu phim Cinestar Cinemas, bao gồm các chi nhánh phụ thuộc, công ty con, công ty thành viên và các tổ chức liên quan mà Cinestar Cinemas có góp vốn đầu tư (gọi chung là Cinestar Cinemas trong Quy Định Chung này). <br />
+      Khi truy cập vào đường dẫn: http://www.ticknow.vn, Quý Khách Hàng cần đảm bảo có độ tuổi phù hợp theo quy định của pháp luật. Trong trường hợp là trẻ em hoặc người chưa thành niên, vui lòng chỉ truy cập website khi có sự đồng ý của người giám hộ hợp pháp. <br />
+      Bằng việc truy cập, sử dụng và thao tác trên website, Quý Khách Hàng đồng ý chấp nhận toàn bộ nội dung của Quy Định Chung và các chính sách bổ sung được cập nhật liên tục. Nếu Quý Khách Hàng không đồng ý với các điều khoản này, vui lòng không sử dụng website.
+      </p>
+      <p>Xin Quý Khách Hàng vui lòng đọc kỹ những Quy Định Chung, bao gồm:</p>
+      <ul className="list-decimal list-inside">
+        <li >MIỄN TRỪ TRÁCH NHIỆM <br />
+            <p className=" pl-4 ">Khi truy cập và sử dụng website, Quý Khách Hàng đồng ý tự chịu mọi rủi ro. TickNow  không chịu trách nhiệm về bất kỳ tổn thất nào có thể phát sinh trực tiếp hoặc gián tiếp do việc truy cập hoặc tải dữ liệu từ website về thiết bị cá nhân.</p>
+        </li>
+        <li>TRÁCH NHIỆM CỦA NGƯỜI SỬ DỤNG <br />
+            <p className=" pl-4 ">TickNow  mong muốn Quý Khách Hàng tôn trọng và nghiêm túc chấp hành các quy định pháp luật về việc sử dụng website thương mại điện tử, cũng như toàn bộ các Quy Định Chung đã đề cập.</p>
+            <p className=" pl-4 ">Trường hợp Quý Khách Hàng được yêu cầu cung cấp thông tin cá nhân, các thông tin cung cấp phải chính xác và phản ánh đúng thực tế. TickNow không chịu trách nhiệm đối với những trường hợp thông tin được cung cấp không đúng sự thật, dù là do vô ý hay cố ý.</p>
+        </li>
+        <li>NỘI DUNG WEBSITE <br />
+            <p className=" pl-4 ">
+                Tất cả những thông tin được đăng tải trên website được cung cấp cho Quý Khách Hàng một cách trung thực, tuy nhiên, TickNow không kèm theo bất kỳ cam kết nào. TickNow không bảo đảm, hay có bất kỳ tuyên bố nào liên quan đến tính chính xác, độ xác thực, độ tin cậy của việc sử dụng hay kết quả của việc sử dụng nội dung trên website này. TickNow luôn cố gắng cập nhật toàn bộ thông tin một cách thường xuyên, kịp thời nhưng TickNow không bảo đảm rằng những nội dung hiện tại là mới nhất, chính xác hay đầy đủ nhất.  <br />
+                Tất cả các nội dung website có thể được thay đổi bất kỳ lúc nào. TickNow bảo lưu quyền thay đổi, chỉnh sửa và loại bỏ thông tin vào bất kỳ thời điểm nào. Quý Khách Hàng vui lòng cập nhật thường xuyên website để biết những thay đổi mới nhất. <br />
+                Tất cả thông tin, bao gồm nhưng không giới hạn: nội dung, hình ảnh, phần mềm, dữ liệu… được đăng tải lên website và các sản phẩm liên quan của TickNow đều thuộc bản quyền của TickNow, Quý Khách Hàng có thể tải về và sử dụng trong phạm vi pháp luật cho phép. <br />
+                Quý Khách Hàng có thể tải về, chuyển tiếp hoặc phương thức khác nhằm sử dụng những thông tin được đăng tải trên website của TickNow để chia sẻ, phổ biến đến công chúng với điều kiện phải trích rõ nguồn và chủ sở hữu thông tin. <br />
+                Cá nhân và tổ chức không được phép sử dụng nội dung trên website này với mục đích thương mại mà không có sự chấp thuận của TickNow bằng văn bản.
+            </p>
+        </li>
+        <li>BẢO MẬT THÔNG TIN <br />
+            <p className="pl-4">Việc thu thập, sử dụng, bảo mật thông tin của Quý Khách Hàng được quy định cụ thể tại Chính Sách Bảo Mật Thông Tin được đăng tải trên website này. <br />
+            TickNow cam kết không bán, chuyển nhượng, chuyển giao, trao đổi để nhận lại lợi ích khác, hoặc thực hiện các hành vi nhằm chuyển quyền sở hữu thông tin cá nhân của Quý Khách Hàng.</p>
+        </li>
+        <li>NỘI DUNG TẢI LÊN CỦA KHÁCH HÀNG <br />
+            <p className="pl-4">
+                TickNow không chịu trách nhiệm sàng lọc, chỉnh sửa hoặc giám sát nội dung được Quý Khách Hàng đăng tải lên website và các sản phẩm liên quan (nếu có), cũng như không thể đảm bảo tính chính xác của những ý kiến, bình luận này. Nếu nhận được thông tin về những vi phạm, gây tổn hại hoặc bất hợp pháp, TickNow có quyền xác minh và quyết định chấm dứt cung cấp dịch vụ đối với thành viên vi phạm. <br />
+                Quý Khách Hàng không được đưa lên, hoặc chuyển tải lên website những hình ảnh, từ ngữ khiêu dâm, thô tục, xúc phạm, phỉ báng, bôi nhọ, đe dọa, những thông tin không hợp pháp hoặc những thông tin có thể đưa đến việc vi phạm pháp luật, trách nhiệm pháp lý. TickNow không chịu trách nhiệm hoặc có nghĩa vụ pháp lý đối với những phát sinh từ nội dung do quý khách hàng tải lên website.  <br />
+                Mặc dù đã có những quy định nêu trên, TickNow không thể bảo đảm chỉnh sửa hoặc xoá bỏ lập tức những nội dung vi phạm. Cũng như TickNow không phải chịu trách nhiệm pháp lý đối với những nội dung do Quý Khách Hàng đăng tải trên website của TickNow. Đồng thời, quý khách hàng cũng có trách nhiệm cho mối liên hệ giữa quý khách hàng và những người dùng khác. TickNow có quyền, nhưng không có nghĩa vụ theo dõi các tranh chấp giữa các người dùng với nhau. 
+            </p>
+        </li>
+        <li>BẢN QUYỀN WEBSITE <br />
+            <p className="pl-4">
+                TickNow là chủ sở hữu duy nhất bản quyền của website này và các sản phẩm liên quan. TickNow có quyền chỉnh sửa, thay đổi, sắp xếp lại nội dung website và các sản phẩm liên quan. Việc chỉnh sửa, thay đổi, sắp xếp lại hoặc tái sử dụng những nội dung trong website này vì bất kỳ mục đích nào khác mà không có sự đồng ý của TickNow đều là vi phạm quyền lợi hợp pháp của TickNow. <br />
+                TickNow luôn tôn trọng sở hữu trí tuệ của người khác, và TickNow kính mong Quý Khách Hàng của TickNow cũng làm như vậy. TickNow luôn cố gắng đảm bảo những dữ liệu trên website và các sản phẩm liên quan đều là hợp pháp, tuy nhiên, TickNow không chắc chắn có thể kiểm soát tất cả thông tin trên website và các sản phẩm liên quan. Khi phát hiện được bất kỳ hành vi vi phạm bản quyền nào trên website và các sản phẩm liên quan, TickNow sẽ xoá nội dung đó khỏi website trong một thời hạn hợp lý.
+            </p>
+        </li>
+        <li>LIÊN KẾT ĐẾN WEBSITE KHÁC <br />
+            <p className="pl-4">
+                Website này có thể được liên kết với những website khác, TickNow không trực tiếp hay gián tiếp quản lý những website liên kết. TickNow không hợp tác, tài trợ, xác thực, đứng sau hay sát nhập với những website đó, trừ khi sự hợp tác đó được công bố rõ ràng. Khi truy cập vào website, TickNow hy vọng quý khách hàng có thể hiểu rằng TickNow không kiểm soát, quản lý những trang liên kết và không chịu trách nhiệm về nội dung của bất kỳ website liên kết nào. 
+            </p>
+        </li>
+        <li>LUẬT ÁP DỤNG VÀ GIẢI QUYẾT TRANH CHẤP <br />
+            <p className="pl-4">
+                Quy Định Chung sẽ được phân tích, diễn giải và giải thích theo quy định của pháp luật nước Cộng Hòa Xã Hội Chủ Nghĩa Việt Nam.  <br />
+                Mọi tranh chấp, khác biệt, khiếu nại phát sinh từ/hoặc liên quan đến nội dung tại website này sẽ được ưu tiên giải quyết thông qua thương lượng, hòa giải. Trường hợp các bên không tự giải quyết, tranh chấp sẽ được đưa ra giải quyết tại Tòa án có thẩm quyền tại Thành phố Hồ Chí Minh. 
+            </p>
+        </li>
+        <li>TÍNH RIÊNG LẺ  <br />
+            <p className="pl-4">
+                Nếu bất kì điều khoản nào trong Quy Định Chung này không hợp pháp, bị bãi bỏ, hoặc vì bất kỳ lý do nào không thể thực thi theo pháp luật, thì điều khoản đó sẽ được tách ra khỏi các điều khoản và điều kiện này và sẽ không ảnh hưởng tới hiệu lực cũng như tính thi hành của bất kỳ điều khoản còn lại nào cũng như không ảnh hưởng tới hiệu lực cũng như tính thi hành của điều khoản sẽ được xem xét theo luật.
+            </p>
+        </li>
+        <li>THÔNG TIN LIÊN HỆ <br />
+            <p className="pl-4">
+                Bất kỳ lúc nào Quý Khách Hàng có bất kỳ câu hỏi, cần hỗ trợ, cần giải thích, khiếu nại hoặc quan tâm về việc Quy Định Chung của TickNow hoặc các giao dịch của Quý Khách Hàng với TickNow, xin vui lòng liên hệ: <br />
+                NHÓM SINH VIÊN TICKNOW <br />
+                Trường Cao đẳng FPT Polytechnic Hồ Chí Minh <br />
+                Địa chỉ: Công viên phần mềm Quang Trung, Quận 12, TP.HCM <br />
+                Email: contact@ticknow.vn <br />
+                Hotline-Zalo: 0833703639
+            </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default privacyPolicy;
