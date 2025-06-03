28c1594f823d8ae5b13d1893672acdb1bec18139
lam-2005 <lam2k5phan@gmail.com>
2025-06-03 18:20:59 +0700

    admin navbar + table components/ lâm

---
+++ client/src/configs/navigation/admin.config.tsx
@@ -0,0 +1,73 @@
+import { TfiDashboard } from "react-icons/tfi";
+import { FaRegStar, FaRegUser } from "react-icons/fa6";
+import { TbBrandCarbon } from "react-icons/tb";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { ReactNode } from "react";
+import { SiGoogleclassroom } from "react-icons/si";
+import { IoTicketOutline, IoTimeOutline } from "react-icons/io5";
+import { PiFilmSlateDuotone } from "react-icons/pi";
+export default interface linkInterface {
+  id: number;
+  title: string;
+  url: string;
+  icon: ReactNode;
+}
+const links: linkInterface[] = [
+  {
+    id: 1,
+    title: "Thống kê",
+    url: "/admin",
+    icon: <TfiDashboard />,
+  },
+
+  {
+    id: 6,
+    title: "Phim chiếu",
+    url: "/admin/movie",
+    icon: <PiFilmSlateDuotone />,
+  },
+  {
+    id: 3,
+    title: "Rạp chiếu",
+    url: "/admin/cinema",
+    icon: <TbBrandCarbon />,
+  },
+  {
+    id: 4,
+    title: "Suất chiếu",
+    url: "/admin/showtime",
+    icon: <IoTimeOutline />,
+  },
+  {
+    id: 7,
+    title: "Phòng chiếu",
+    url: "/admin/room",
+    icon: <SiGoogleclassroom />,
+  },
+  {
+    id: 2,
+    title: "Người dùng",
+    url: "/admin/user",
+    icon: <FaRegUser />,
+  },
+  {
+    id: 9,
+    title: "Đặt vé",
+    url: "/admin/booking",
+    icon: <IoTicketOutline />,
+  },
+  {
+    id: 8,
+    title: "Đánh giá",
+    url: "/admin/rating",
+    icon: <FaRegStar />,
+  },
+  {
+    id: 5,
+    title: "Khuyến mãi",
+    url: "/admin/vouchers",
+    icon: <MdOutlineLocalOffer />,
+  },
+];
+
+export { links };
