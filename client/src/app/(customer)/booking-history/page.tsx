6f9075697a4dc114c08a4448093b1435d13310ab
lam-2005 <lam2k5phan@gmail.com>
2025-06-11 17:58:13 +0700

    fix duplicate

--- client/src/app/(customer)/booking-history/page.tsx
+++ client/src/app/(customer)/booking-history/page.tsx
@@ -3,18 +3,15 @@ import TicketPopup from "@/components/Popup/TicketPopup";
 import usePopup from "@/hooks/usePopup";
 import { MovieType } from "@/interfaces/movie.interface";
 import { useRouter } from "next/navigation";
-import React from "react";
-import TicketPopup from "@/components/Popup/TicketPopup";
-import { MovieType } from "@/interfaces/movie.interface";
-import usePopup from "@/hooks/usePopup";
+
 const Profile = () => {
   const router = useRouter();
 
-  const {ticketPopup, openTicket, closeTicket} = usePopup()
-   return (
+  const { ticketPopup, openTicket, closeTicket } = usePopup();
+  return (
     <div className="container flex-column items-center mt-10 gap-10">
       {ticketPopup && (
-        <TicketPopup info={{} as MovieType} onClose={closeTicket}/>
+        <TicketPopup info={{} as MovieType} onClose={closeTicket} />
       )}
       <h2>Thông tin cá nhân</h2>
       <div className="space-x-10">
@@ -45,7 +42,12 @@ const Profile = () => {
               <td className="py-2">01/01/2023</td>
               <td className="py-2 line-clamp-1">Phim chiếu rạp</td>
               <td className="py-2">
-                <button className="text-primary hover:underline" onClick={openTicket}>Xem</button>
+                <button
+                  className="text-primary hover:underline"
+                  onClick={openTicket}
+                >
+                  Xem
+                </button>
               </td>
               <td className="py-2">
                 <button className="text-primary hover:underline">
