8227a82821ba65372085359fd13cc2098e5c6755
trangioi0412 <trangioi479@gmail.com>
2025-06-16 16:08:02 +0700

    Update movies.js

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,26 +1,8 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express');
+const router = express.Router()
 
-const movieControler = require("../controler/movies.controler");
+const movieControler = require('../controler/movies.controler');
 
-<<<<<<< HEAD
-router.get("/", async (req, res, next) => {
-  try {
-    const movies = await movieControler.getMovies();
-    console.log(movies);
-    if (movies) {
-      return res
-        .status(200)
-        .json({
-          data: movies,
-          status: true,
-          message: "Lấy dữ liệu thành công",
-        });
-    } else {
-      return res
-        .status(404)
-        .json({ status: false, message: "Lấy dữ liệu thất bại" });
-=======
 router.get('/', async (req,res,next) => {
     try{
         const { status } = req.query;
@@ -40,19 +22,9 @@ router.get('/', async (req,res,next) => {
     }catch(error){
         console.error(error);
         return res.status(500).json({status: false, message: 'Lấy dữ liệu movie thất bại'})
->>>>>>> VanGioiPS42100
     }
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ status: false, message: "Lấy dữ liệu movie thất bại" });
-  }
-});
+})
 
-<<<<<<< HEAD
-module.exports = router;
-=======
 router.get('/:id', async (req,res,next) => {
     try {
         
@@ -62,4 +34,3 @@ router.get('/:id', async (req,res,next) => {
 })
 
 module.exports = router
->>>>>>> VanGioiPS42100
