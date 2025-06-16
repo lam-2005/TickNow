f63073f0a81930db7307ec2fcfe37546eff7561b
trangioi0412 <trangioi479@gmail.com>
2025-06-16 15:08:43 +0700

    fix(port)/gioitv2/fix_bug_port&feat(new_api)/gioitv/creat_new_api

--- server/controler/movies.controler.js
+++ server/controler/movies.controler.js
@@ -12,19 +12,20 @@ const getMovies = async () => {
 }
 
 const getMovieStatus = async (status) => {
-    try{
-
-        if(!status){
+    try {
+        const validStatus = [ "Đang Chiếu", "Sắp Chiếu" ];
+        console.log(status);
+        if (!status || !validStatus.includes(status)) {
             throw new Error("❌ Trạng thái phim không hợp lệ");
         }
 
-        const movie = await movieModel.find({status})
-        
-        return movie
-    }catch(error){
-        console.error(error.message)
-        throw new Error('❌ Lỗi lấy dữ liệu của movie')
+        const movies = await movieModel.find({ status });
+        return movies;
+    } catch (error) {
+        console.error(error.message);
+        throw new Error('❌ Lỗi lấy dữ liệu của movie');
     }
-}
+};
+
 
 module.exports = { getMovies, getMovieStatus };
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -5,21 +5,20 @@ const movieControler = require('../controler/movies.controler');
 
 router.get('/', async (req,res,next) => {
     try{
-        const result = await movieControler.getMovies();
+        const { status } = req.query;
+        let result
+
+        if(status){
+            result = await movieControler.getMovieStatus(status);
+        }else{
+            result = await movieControler.getMovies();
+        }
+
         if(result){
             return res.status(200).json({ data: result ,status: true, message: 'Lấy dữ liệu thành công'})
         }else{
             return res.status(404).json({ status: false, message: 'Lấy dữ liệu thất bại' })
         }
-    }catch(error){
-        console.error(error);
-        return res.status(500).json({status: false, message: 'Lấy dữ liệu movie thất bại'})
-    }
-})
-
-router.get('/active', async (req, res) => {
-    try{
-        const result = await movieControler.getMovieStatus()
     }catch(error){
         console.error(error);
         return res.status(500).json({status: false, message: 'Lấy dữ liệu movie thất bại'})
