aaee4534c43afc7614bd2e2322c4e35dbb0e32bf
trangioi0412 <trangioi479@gmail.com>
2025-06-17 16:33:15 +0700

    feact(service)/gioitv/create/service_screening

--- server/controler/movies.controler.js
+++ server/controler/movies.controler.js
@@ -1,24 +1,50 @@
-const movieServiece = require('../service/movie.service')
+const { default: mongoose } = require('mongoose');
+const movieServiece = require('../service/movie.service');
+const dayjs = require('dayjs');
 
 const getMovies =async (req,res,next) => {
     try{
-        const { status } = req.query;
-        const limit = parseInt(req.query.limit);
-        const page = parseInt(req.query.page);
-        console.log(limit, page);
-        let result
-        
-        if(status){
-            result = await movieServiece.getMovieStatus(status, limit, page);
-        }else{
-            result = await movieServiece.getMovies(limit, page);
-        }
+        // query host
+        const { status, date, genre } = req.query;
 
+        const limit = parseInt(req.query.limit);
+
+        const page = parseInt(req.query.page);
+
+        // create variable storage
+        let filter = { };
+
+        let result
+
+        // check variable  
+        if (status) filter.status = status;
+
+        if(date){
+            const parsedDate = dayjs(date, 'MM-DD-YYYY');
+            if(parsedDate.isValid()){
+
+                const start = parsedDate.startOf('day').toDate();
+                const end = parsedDate.endOf('day').toDate();
+
+                filter.release_date = {
+                    $gte: start,
+                    $lte: end
+                };
+            }else{
+                console.warn('⚠️ Ngày không hợp lệ:', date);
+            }
+        }   
+
+        // get data
+        result = await movieServiece.getMovies(filter, limit, page);
+
+        // check data
         if(result){
             return res.status(200).json({ data: result ,status: true, message: 'Lấy dữ liệu thành công'})
         }else{
             return res.status(404).json({ status: false, message: 'Lấy dữ liệu thất bại' })
         }
+
     }catch(error){
         console.error(error);
         return res.status(500).json({status: false, message: 'Lấy dữ liệu movie thất bại'})
@@ -28,7 +54,7 @@ const getMovies =async (req,res,next) => {
 const getDetailMovie = async (req,res,next) => {
     try {
         const { id } = req.params;
-        let result = await movieControler.getDetailMovie(id);
+        let result = await movieServiece.getDetailMovie(id);
         if(result){
             return res.status(200).json({ data: result ,status: true, message: 'Lấy dữ liệu thành công'})
         }else{
