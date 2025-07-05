4404febcbb2c3dc0beb39d772317e0160f6cf050
ntq1311 <151548272+ntq1311@users.noreply.github.com>
2025-07-05 20:08:20 +0700

    popupContainer

--- client/src/admin_components/ScreenManagementComponents/UpdateForm/UpdateFormContainer.tsx
+++ client/src/admin_components/ScreenManagementComponents/UpdateForm/UpdateFormContainer.tsx
@@ -1,33 +1,6 @@
-// "use client";
-// import React from "react";
-// import PopupContainer from "../../PopupContainer";
-// import UpdateForm from "./UpdateForm";
-// import { ScreenReq, Screening } from "@/interfaces/screening.interface";
-
-// type Props = {
-//   info: Screening;
-//   closeForm: () => void;
-//   onSubmit: (data: ScreenReq) => void;
-// };
-
-// const UpdateFormContainer = ({ info, closeForm, onSubmit }: Props) => {
-//   return (
-//     <PopupContainer title="✏️ Cập nhật suất chiếu" closeForm={closeForm}>
-//       <UpdateForm
-//         data={info}
-//         onCancel={closeForm}
-//         onSubmit={onSubmit}
-//         onlyEditStatusAndRole={true} // ✅ chỉ cho chỉnh status và role
-//       />
-//     </PopupContainer>
-//   );
-// };
-
-// export default UpdateFormContainer;
-
 "use client";
 import React from "react";
-import PopupContainer from "@/components/Popup/PopupContainer"; // ✅ đã sửa
+import PopupContainer from "../../PopupContainer";
 import UpdateForm from "./UpdateForm";
 import { ScreenReq, Screening } from "@/interfaces/screening.interface";
 
@@ -39,16 +12,43 @@ type Props = {
 
 const UpdateFormContainer = ({ info, closeForm, onSubmit }: Props) => {
   return (
-    <PopupContainer onClose={closeForm}>
+    <PopupContainer title="✏️ Cập nhật suất chiếu" closeForm={closeForm}>
       <UpdateForm
         data={info}
         onCancel={closeForm}
         onSubmit={onSubmit}
-        onlyEditStatusAndRole={true}
+        onlyEditStatusAndRole={true} // ✅ chỉ cho chỉnh status và role
       />
     </PopupContainer>
   );
 };
 
 export default UpdateFormContainer;
+
+// "use client";
+// import React from "react";
+// import PopupContainer from "@/components/Popup/PopupContainer"; // ✅ đã sửa
+// import UpdateForm from "./UpdateForm";
+// import { ScreenReq, Screening } from "@/interfaces/screening.interface";
+
+// type Props = {
+//   info: Screening;
+//   closeForm: () => void;
+//   onSubmit: (data: ScreenReq) => void;
+// };
+
+// const UpdateFormContainer = ({ info, closeForm, onSubmit }: Props) => {
+//   return (
+//     <PopupContainer onClose={closeForm}>
+//       <UpdateForm
+//         data={info}
+//         onCancel={closeForm}
+//         onSubmit={onSubmit}
+//         onlyEditStatusAndRole={true}
+//       />
+//     </PopupContainer>
+//   );
+// };
+
+// export default UpdateFormContainer;
 
