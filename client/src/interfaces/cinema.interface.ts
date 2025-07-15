8b22d2510ecc0e659c7f2701b8ea13c419c73168
lam-2005 <lam2k5phan@gmail.com>
2025-07-16 03:16:29 +0700

    update cinema interface

--- client/src/interfaces/cinema.interface.ts
+++ client/src/interfaces/cinema.interface.ts
@@ -36,3 +36,18 @@ export interface LocationRes {
   _id: string;
   name: string;
 }
+export interface Location {
+  _id: string;
+  name: string;
+}
+
+export interface Cinema {
+  _id: string;
+  name: string;
+  image: string;
+  location: {
+    id_location: string;
+    deatil_location: string;
+  };
+  status: number;
+}
