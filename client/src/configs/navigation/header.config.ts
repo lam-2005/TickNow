dc3fd47f7082a5558a0b12c91a19de9e42b9811d
lam-2005 <lam2k5phan@gmail.com>
2025-08-05 02:01:20 +0700

    add link nav

--- client/src/configs/navigation/header.config.ts
+++ client/src/configs/navigation/header.config.ts
@@ -5,4 +5,5 @@ export const linkNavbar: LinkNavbarType[] = [
   { id: 2, url: "/movies", name: "Phim chiếu" },
   { id: 3, url: "/showtimes", name: "Lịch chiếu" },
   { id: 4, url: "/cinema", name: "Rạp chiếu" },
+  { id: 5, url: "/post", name: "Khuyến mãi và ưu đãi" },
 ];
