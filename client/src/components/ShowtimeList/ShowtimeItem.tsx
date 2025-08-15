e4888f8f2db5fc9b9cbe217e6a65be46943d9842
lam-2005 <lam2k5phan@gmail.com>
2025-08-15 23:51:36 +0700

    fix update and add shotime

--- client/src/components/ShowtimeList/ShowtimeItem.tsx
+++ client/src/components/ShowtimeList/ShowtimeItem.tsx
@@ -6,7 +6,7 @@ const ShowtimeItem = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="space-y-2.5 not-first:before:w-full not-first:before:h-px before:bg-white before:absolute before:-translate-y-[15px]">
+    <div className="space-y-2.5 not-first:before:w-full not-first:before:h-px not-first:before:mt-4  before:bg-white before:absolute before:-translate-y-[15px]">
       <h3 className="font-semibold">{nameCinema}</h3>
       <div className="space-y-2.5">{children}</div>
     </div>
