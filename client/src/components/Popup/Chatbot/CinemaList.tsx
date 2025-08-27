da4e4c4ec4a952f1746e7b0fd9c2b9d0800164b6
lam-2005 <lam2k5phan@gmail.com>
2025-08-27 23:32:31 +0700

    update chatbot

--- client/src/components/Popup/Chatbot/CinemaList.tsx
+++ client/src/components/Popup/Chatbot/CinemaList.tsx
@@ -1,13 +1,46 @@
+import env from "@/configs/environment";
 import { Cinema } from "@/interfaces/cinema.interface";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const CinemaList = ({ cinemas }: { cinemas: Cinema[] }) => {
+  const [posterSrc, setPosterSrc] = useState(
+    cinemas[0]?.image ? `${env.IMG_API_URL}${cinemas[0].image}` : "/images.jpg"
+  );
+  if (cinemas.length === 1)
+    return (
+      <div className="w-full bg-black/10 rounded-md shadow-md overflow-hidden">
+        {/* Phần ảnh nền */}
+        <div className="relative w-full h-40">
+          <Image
+            src={posterSrc}
+            alt={`Ảnh rạp ${cinemas[0].name}`}
+            fill
+            priority
+            className="object-cover"
+            sizes="400px"
+            onError={() => setPosterSrc("/images.jpg")}
+          />
+        </div>
+
+        {/* Phần thông tin */}
+        <div className="p-3">
+          <h1 className="text-background font-bold text-base">
+            {cinemas[0].name}
+          </h1>
+          <p className="text-background text-sm leading-relaxed mt-2.5">
+            {cinemas[0].location.deatil_location}
+          </p>
+          {/* <Button title="Xem chi tiết" className="mt-5" /> */}
+        </div>
+      </div>
+    );
   return (
-    <ul className=" mt-2 max-w-[90%] w-fit rounded-xl bg-black/10 p-2.5 space-y-4">
+    <ul className="list-disc list-inside mt-2 max-w-[90%] w-fit rounded-xl bg-black/10 p-2.5 space-y-4">
       {cinemas.map((cinema) => (
         <li
           key={cinema._id}
-          className="hover:underline hover:text-blue-500 text-background text-sm break-words whitespace-pre-line "
+          className="text-background text-sm break-words whitespace-pre-line "
         >
           {cinema.name}
         </li>
